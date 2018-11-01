0913cb5be25a518c9d426ce52ebc46c1d07c859f
lvrnjak1 <1lamsaetf>
2018-11-01 17:57:42 +0100

    napravljena apstraktna klasa TelefonskiBroj

---
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public abstract class TelefonskiBroj {
+    public abstract String ispisi();
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
