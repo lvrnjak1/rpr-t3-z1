6d291a3e5efe7d88e39c698ca8db5deede771bdf
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2018-11-02 16:38:48 +0100

    dodala enum

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -1,5 +1,17 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
 public class FiksniBroj {
+
+    public enum Grad{
+        BUGOJNO("030"), ODŽAK("031"), BREZA("032"), SARAJEVO("033"), TUZLA("035"), MOSTAR("036"), BIHAĆ("037"), GORAŽDE("038"), ZENICA("032");
+
+        private final String pozivniBroj;
+
+        Grad(String pozivniBroj) {
+            this.pozivniBroj = pozivniBroj;
+        }
+    }
+
+    
     
 }
