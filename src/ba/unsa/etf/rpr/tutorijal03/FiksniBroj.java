af63e8195736b485799367fcc52695cade077d03
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2018-11-02 16:53:21 +0100

    unaprijedjen enum i fiksni broj

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -2,16 +2,45 @@ package ba.unsa.etf.rpr.tutorijal03;
 
 public class FiksniBroj {
 
+
+
     public enum Grad{
-        BUGOJNO("030"), ODŽAK("031"), BREZA("032"), SARAJEVO("033"), TUZLA("035"), MOSTAR("036"), BIHAĆ("037"), GORAŽDE("038"), ZENICA("032");
+        BUGOJNO("030"), BREZA("032"), SARAJEVO("033"), TUZLA("035"), MOSTAR("036"), BIHAC("037"), GORAŽDE("038"),
+        ZENICA("032"), TRAVNIK("030"), ORASJE("031"), LIVNO("034"), SIROKI_BRIJEG("039"), BRCKO("049");
 
         private final String pozivniBroj;
 
         Grad(String pozivniBroj) {
             this.pozivniBroj = pozivniBroj;
         }
+
+        public String getPozivniBroj() {
+            return pozivniBroj;
+        }
     }
 
-    
+    private Grad grad;
+    private String broj;
+
+    FiksniBroj(Grad grad, String broj){
+        setGrad(grad);
+        setBroj(grad.getPozivniBroj() + "/" + broj);
+    }
+
+    public Grad getGrad() {
+        return grad;
+    }
+
+    public void setGrad(Grad grad) {
+        this.grad = grad;
+    }
+
+    public String getBroj() {
+        return broj;
+    }
+
+    public void setBroj(String broj) {
+        this.broj = broj;
+    }
     
 }
